ba4b18c598fc34fa149272ca0a15aa4dd9295683
MoffunW <moffuncan@gmail.com>
2025-01-14 22:28:38 +0300

    feat: try deploy to work

--- bot.js
+++ bot.js
@@ -1,17 +1,48 @@
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
+const { Groq } = require("groq-sdk");
 
+const groq = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+async function getGroqChatCompletion(userInput) {
+  console.log(userInput);
+  try {
+    const completion = await groq.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content:
+            "Ты рассказчик бредовых историй, я отправляю тебе текст как начало истории, а ты её продолжаешь в манере нонсенс",
+        },
+        {
+          role: "user",
+          content: userInput,
+        },
+      ],
+      model: "llama-3.3-70b-versatile",
+    });
+
+    return completion.choices[0]?.message?.content || "Got empty message";
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 bot.on("sticker", (ctx) => ctx.reply("👍"));
 bot.on("text", (ctx) => {
   (async function () {
-    console.log(ctx.message.text);
     const userInput = ctx.message.text;
+    console.log(userInput, "input");
 
-    const response = 1;
+    const response = await getGroqChatCompletion(userInput);
 
-    ctx.reply(response + " " + userInput);
+    console.log(response, "response");
+    if (!response) return console.log("No response");
+    ctx.reply(response);
   })();
 });
 
