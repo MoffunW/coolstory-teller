d03c1246ed162b4614dbf94364e5f380fac9f7f7
MoffunW <moffuncan@gmail.com>
2025-01-15 11:17:37 +0300

    feat: return express

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const { Telegraf, session } = require("telegraf");
 const { Groq } = require("groq-sdk");
+const express = require("express");
 const { interleaveArrays } = require("./utils.js");
 
 const groq = new Groq({
@@ -94,7 +95,7 @@ async function getGroqChatCompletion(ctx, userInput) {
 
     return completion.choices[0]?.message?.content || "Got empty message";
   } catch (error) {
-    ctx.reply("Произошла ошибка в генерации: ", error);
+    ctx.reply("Произошла ошибка в генерации", error);
     console.error(error);
   }
 }
@@ -145,6 +146,16 @@ bot.on("text", (ctx) => {
 bot.on("sticker", (ctx) => ctx.reply("👍"));
 
 bot.launch();
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("working");
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
 
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
